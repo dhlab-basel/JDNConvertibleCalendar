46064755c608aa9807c6ff6005ec8ef3cbd641ce
Tobias Schweizer <t.schweizer@unibas.ch>
2020-09-08 09:01:12 +0200

    refactor (index.ts): simplify exports

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,5 @@ export { CalendarDate } from "./CalendarDate";
 export { CalendarPeriod } from "./CalendarPeriod";
 export { JDNPeriod } from "./JDNPeriod";
 export { JDNConvertibleCalendar, GregorianCalendarDate, IslamicCalendarDate, JulianCalendarDate } from "./JDNConvertibleCalendar";
-
-import { JDNConvertibleConversionModule } from './JDNCalendarConversion';
-export { JDNConvertibleConversionModule }
+export { JDNConvertibleConversionModule } from './JDNCalendarConversion';
+export { JDNConvertibleCalendarNames } from './JDNCalendarNames';
